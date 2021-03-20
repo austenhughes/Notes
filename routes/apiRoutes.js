bafecf45ba662ec44279f8a303decda225fa7cf4
Austn Hughes <austen.hughes.91@gmail.com>
2021-03-19 20:52:39 -0600

    writing notes and laoding page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,15 +4,15 @@ const fs = require('fs');
 module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => {
-        let getNotes = fs.readFileSync('./db/db.json','utf-8');
-        res.json(getNotes)
-        // res.json(saved);
+        console.log(saved)
+        return res.json(saved) 
     });
     
-    // app.post('/api/notes', (req, res) => {
-    //     const addNote = req.body
-    //     saved.push(addNote);
-    //     res.json(addNote);   
-    // })
+    app.post('/api/notes', (req, res) => {
+        const addNote = req.body
+        saved.push(addNote);
+        fs.writeFileSync(db.json, JSON.stringify(saved));
+        res.json(addNote);   
+    })
 
 };
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// const saved = require('./db/db.json');
 const express = require('express');
 const app = express();
 
@@ -5,10 +6,11 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+  console.log(`App listening on PORT: ${PORT}`);
   });
