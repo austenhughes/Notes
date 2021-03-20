2d480764feab47ba972bc2cba55093814a3dd245
Austn Hughes <austen.hughes.91@gmail.com>
2021-03-19 19:01:49 -0600

    embeeded javascript and css may come back to how to do this corectly later works for now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,18 @@
 const saved = require('../db/db.json');
+const fs = require('fs');
 
 module.exports = (app) => {
 
-    app.get('/api/notes', (req, res) => res.json(saved)); 
+    app.get('/api/notes', (req, res) => {
+        let getNotes = fs.readFileSync('./db/db.json','utf-8');
+        res.json(getNotes)
+        // res.json(saved);
+    });
     
-    app.post('/api/notes', (req, res) => {
-        const addNote = req.body
-        saved.push(addNote);
-        res.json(addNote);   
-    })
+    // app.post('/api/notes', (req, res) => {
+    //     const addNote = req.body
+    //     saved.push(addNote);
+    //     res.json(addNote);   
+    // })
 
 };
