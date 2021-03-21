710c00f08f1e76a38ce6a1453b2e9a1f4e96a385
Austn Hughes <austen.hughes.91@gmail.com>
2021-03-20 18:46:43 -0600

    woorking delete rout

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,32 @@
-const saved = require('../db/db.json');
+let saved = require('../db/db.json');
 const fs = require('fs');
-const uuid = require('uuidv4');
+const { uuid } = require('uuidv4');
+
 
 module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => {
-        console.log(saved)
         return res.json(saved) 
     });
     
     app.post('/api/notes', (req, res) => {
-
         let addNote = req.body
-        let idNumber = (saved.length)
-        addNote.id = idNumber
-        // addNote.id = uuidv4();
+        let idNumber = uuid();
+        addNote.id = idNumber;
         saved.push(addNote);
         fs.writeFileSync('./db/db.json', JSON.stringify(saved));
         res.json(addNote); 
-        // console.log(saved)  
+    });
 
-    })
+    app.delete("/api/notes/:id", (req, res) => {
+        let UniqId = (req.params.id);
+        console.log(UniqId);
+
+        saved = saved.filter(x =>
+        x.id != UniqId); 
+              
+        fs.writeFileSync("./db/db.json", JSON.stringify(saved));
+        return res.json(saved);
+    });
 
 };
